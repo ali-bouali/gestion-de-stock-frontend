269b88e45cf6c12882687fd83d4c46194c14f4e6
aliboualiporsche <ali.bouali@mimacom.com>
2021-04-07 16:36:33 +0200

    Finalizing frontend screens

--- src/app/composants/boutton-action/boutton-action.component.ts
+++ src/app/composants/boutton-action/boutton-action.component.ts
@@ -27,14 +27,4 @@ export class BouttonActionComponent implements OnInit {
     this.clickEvent.emit();
   }
 
-
-  // private static void rangeCheck(int arrayLen, int fromIndex, int toIndex) {
-  //   if (fromIndex > toIndex)
-  //     throw new IllegalArgumentException("fromIndex(" + fromIndex +") > toIndex(" + toIndex+")");
-  //   if (fromIndex < 0)
-  //     throw new ArrayIndexOutOfBoundsException(fromIndex);
-  //   if (toIndex > arrayLen)
-  //     throw new ArrayIndexOutOfBoundsException(toIndex);
-  // }
-
 }
